fc859a67f660df8337cfba71d51b2e47df23c862
unknown <abiramibalakumaran@gmail.com>
2021-04-22 13:31:12 +0530

    first commit

---
+++ Pgm1.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int a=3;
+        int b=1;
+Console.WriteLine("Sum of "+a"+"+b"="+(a+b)");
+        Console.WriteLine(a+b);
+         Console.WriteLine(a-b);
+          Console.WriteLine(a/b);
+           Console.WriteLine(a==b);
+            Console.WriteLine(a*b);
+        Console.ReadLine();
+    }
+}
